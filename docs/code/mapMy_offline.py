3f61f553dcabe5de2721b7778c0716169e9d3eaa
adumans <buaadjh@163.com>
2017-08-03 14:48:33 +0800

    dilation and eroded

--- docs/code/mapMy_offline.py
+++ docs/code/mapMy_offline.py
@@ -40,18 +40,36 @@ for item in map0:
 #plot
 fig = pyplot.figure(1, figsize=SIZE, dpi=90)
 ax = fig.add_subplot(121)
+
+point0 = waylist[0]['nodeslocation']
+line0 = LineString(point0)
+dilatedAll = line0.buffer(0.0002, cap_style=3)
+
 for oneway in waylist:
     points = oneway['nodeslocation']
     line = LineString(points)
+    dilated = line.buffer(0.0002, cap_style=3)
+    dilatedAll = dilatedAll.union(dilated)
     plot_line(ax, line)
+patch = PolygonPatch(dilatedAll, fc=BLUE, ec=BLUE, alpha=0.5, zorder=2)
+ax.add_patch(patch)
+
+ax = fig.add_subplot(122)
+eroded = dilatedAll.buffer(-0.000005)
+polygon = eroded.__geo_interface__
+patch2 = PolygonPatch(polygon, fc=BLUE, ec=RED, alpha=0.5, zorder=1)
+ax.add_patch(patch2)
+
 set_limits(ax, 39,42,116,119)
 pyplot.show()
 
-for d in waylist:
-    f= open('map_dict', 'a')
-    json.dump(d,f)
-    f.write('\n')
-f.close()
 
-print (nodesinfo)
 
+# for d in waylist:
+#     f= open('map_dict', 'a')
+#     json.dump(d,f)
+#     f.write('\n')
+# f.close()
+
+# print (nodesinfo)
+# print (waylist)
