88e15cf6b7dbfa58b92c37e8ebf2e3e35fe8e290
adumans <buaadjh@163.com>
2017-08-07 21:54:48 +0800

    leftpart and rightpart

--- docs/code/mapMy_offline.py
+++ docs/code/mapMy_offline.py
@@ -5,20 +5,104 @@ from descartes import PolygonPatch
 from figures import SIZE, BLUE, GRAY, RED, set_limits, plot_line
 import json
 MyApi = OsmApi()
-types = ['motorway', 'trunk', 'primary', 'secondary', 'trunk_link', 'motorway_link', 'primary_link', 'tertiary_link', 'traffic_signals', 'bus_stop']
-print ('start collecting map ...')
+types = ['motorway', 'trunk', 'primary', 'secondary','trunk_link', 'motorway_link', 'primary_link', 'tertiary_link', 'traffic_signals', 'bus_stop']
 
-MINLON = 116.2992
-MINLAT = 39.9005
-MAXLON = 116.3555
-MAXLAT = 39.9445
+MINLON0 = 116.1900
+MINLAT0 = 39.9837
+MAXLON0 = 116.3874
+MAXLAT0 = 40.0281
+map0 = MyApi.Map(116.3874,39.9837,116.3874,39.9837)
+
+
+MINLON = 116.1804
+MINLAT = 39.7500
+MAXLON = 116.5567
+MAXLAT = 40.0000
+
+addedlat = 0.0355
+MINLON_1 = 116.1804
+MAXLON_1 = 116.3905
+MINLAT1 = 39.7470
+MAXLAT1 = MINLAT1 + addedlat
+# left part
+for i in range(0, 8):
+    MINLAT_1 = round(MINLAT1 + i * addedlat, 4)
+    MAXLAT_1 = round(MAXLAT1 + i * addedlat, 4)
+    print('start collecting map%d  ...' %(i+1))
+    print(MINLON_1, MINLAT_1, MAXLON_1, MAXLAT_1)
+    # map1 = MyApi.Map(MINLON_1, MINLAT_1, MAXLON_1, MAXLAT_1)
+    # map0.extend(map1)
+    if MAXLAT_1 >= MAXLAT:
+        MAXLAT = MAXLAT_1
+    if MINLAT_1 <= MINLAT:
+        MINLAT = MINLAT_1
+    if MAXLON_1 >= MAXLON:
+        MAXLON = MAXLON_1
+    if MINLON_1 <= MINLON:
+        MINLON = MINLON_1
+#right part
+MINLON_2 = 116.3905
+MAXLON_2 = 116.5567
+MINLAT2 = 39.7470
+MAXLAT2 = MINLAT2 + addedlat
+for i in range(0, 8):
+    MINLAT_2 = round(MINLAT2 + i * addedlat, 4)
+    MAXLAT_2 = round(MAXLAT2 + i * addedlat, 4)
+    print('start collecting map%d  ...' %(i+1))
+    print(MINLON_2, MINLAT_2, MAXLON_2, MAXLAT_2)
+    # map2 = MyApi.Map(MINLON_2, MINLAT_2, MAXLON_2, MAXLAT_2)
+    # map0.extend(map2)
+    if MAXLAT_2 >= MAXLAT:
+        MAXLAT = MAXLAT_2
+    if MINLAT_2 <= MINLAT:
+        MINLAT = MINLAT_2
+    if MAXLON_2 >= MAXLON:
+        MAXLON = MAXLON_2
+    if MINLON_2 <= MINLON:
+        MINLON = MINLON_2
+
+
+
+
+
+
+# MINLON1 = 116.3874
+# MINLAT1 = 39.9837
+# MAXLON1 = 116.5615
+# MAXLAT1 = 40.0281
+#
+# MINLON2 = 116.1900
+# MINLAT2 = 39.9396
+# MAXLON2 = 116.3874
+# MAXLAT2 = 39.9837
+#
+# MINLON3 = 116.3874
+# MINLAT3 = 39.9396
+# MAXLON3 = 116.5701
+# MAXLAT3 = 39.9837
+#
+# MINLON = min(MINLON0, MINLON1, MINLON2, MINLON3)
+# MINLAT = min(MINLAT0, MINLAT1, MINLAT2, MINLAT3)
+# MAXLON = max(MAXLON0, MAXLON1, MAXLON2, MAXLON3)
+# MAXLAT = max(MAXLAT0, MAXLAT1, MAXLAT2, MAXLAT3)
 
 # MINLON = 116.3666
 # MINLAT = 39.9623
 # MAXLON = 116.4469
 # MAXLAT = 39.9937
-
-map0 = MyApi.Map(MINLON, MINLAT, MAXLON, MAXLAT)
+# print ('start collecting map0 ...')
+# map0 = MyApi.Map(MINLON0, MINLAT0, MAXLON0, MAXLAT0)
+# print ('start collecting map1 ...')
+# map1 = MyApi.Map(MINLON1, MINLAT1, MAXLON1, MAXLAT1)
+# print ('start collecting map2 ...')
+# map2 = MyApi.Map(MINLON2, MINLAT2, MAXLON2, MAXLAT2)
+# print ('start collecting map3 ...')
+# map3 = MyApi.Map(MINLON3, MINLAT3, MAXLON3, MAXLAT3)
+#
+# print ('merge ...')
+# map0.extend(map1)
+# map0.extend(map2)
+# map0.extend(map3)
 #map0 = MyApi.Map(116.34384,39.98189,116.34691,39.98352)
 #print (type(map0))
 #for d in map0:
